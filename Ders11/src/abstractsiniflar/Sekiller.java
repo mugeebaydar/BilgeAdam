1a7e150feff72e22200b22d9ebb089c75853f031
muge.baydar <mugeeebaydar@gmail.com>
2019-03-03 14:13:03 +0300

    ders11

---
+++ Ders11/src/abstractsiniflar/Sekiller.java
@@ -0,0 +1,15 @@
+package abstractsiniflar;
+
+//soyut class
+public abstract class Sekiller {
+
+    public abstract double alanHesapla();
+
+    public double cevreHesapla(){
+        return 0;
+    }
+
+
+
+
+}
